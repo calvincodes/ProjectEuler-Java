047ca6d92190daf815d28880bace236069b5d3d6
calvincodes <arpitjain1821@gmail.com>
2018-12-29 14:20:21 +0530

    Problem 1 - Multiples of 3 and 5

---
+++ src/Problem1.java
@@ -0,0 +1,17 @@
+public class Problem1 {
+
+    public static void main(String[] args) {
+
+        long sum = 0;
+        for (int i = 1; i < 1000; i++) {
+
+            if (i % 3 == 0) {
+                sum += i;
+            } else if (i % 5 == 0) {
+                sum += i;
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
